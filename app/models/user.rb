fca6ad975238ea179f1b503676f4e2bfd24abebf
gavinsharkey <gavinsharkey2016@gmail.com>
2020-04-17 15:54:20 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'is invalid'}
   validates :username, presence: true, uniqueness: true
 
   before_create :set_slug
